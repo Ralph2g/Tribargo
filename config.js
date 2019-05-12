0e30aa91bb263ca43b3fa26a489687de7b912a89
JesusDiaz08 <jesus_kdm@hotmail.com>
2019-05-11 22:08:31 -0500

    DAYS_TOKEN y SECRET_TOKEN

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3001, // El puerto puede ser una variable de entorno o el 3000
-    db: process.env.MONGODB || 'mongodb://localhost:27017/tribargo'
+    db: process.env.MONGODB || 'mongodb://localhost:27017/tribargo',
+    DAYS_TOKEN: 14,
+    SECRET_TOKEN: 'ZIuDo7T9%VMY7}zgP'
 }
