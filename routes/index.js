bfb3cdd950fadfd2ef5d11229541408122ccdb2c
JesusDiaz08 <jesus_kdm@hotmail.com>
2019-05-11 22:04:54 -0500

    CRUD de la aplicacion (Hasta ahora solo esta el de la coleccion Bebida)

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 'use strict'
 
+/**
+ * @author:      Diaz Medina Jesus Kaimorts
+ * @version:     1.0
+ * @description: En este script se encuentran los metodos necesarios para
+ *               poder asignar el comportamiento del CRUD utilizando los
+ *               distintos modelos creados, los cuales están contenidos,
+ *               en el @package models. Se divide por secciones cada uno 
+ *               de los modelos.        
+ **/
+
 const express = require('express');
 const BebidaCtrl = require('../controllers/bebida');
+const auth = require('../middlewares/auth');
 const api = express.Router();
 
 /* |=========== CRUD en la coleccion BEBIDA ===========| */
@@ -11,4 +22,9 @@ api.get('/bebida/:bebidaID', BebidaCtrl.getBebida);
 api.put('/bebida/:bebidaID', BebidaCtrl.updateBebida);
 api.delete('/bebida/:bebidaID', BebidaCtrl.deleteBebida);
 
-module.exports = api
+// es de prueba este
+api.get('/private', auth, (req, res) => {
+    res.status(200).send({ message: 'Tienes acceso.' });
+});
+
+module.exports = api;
