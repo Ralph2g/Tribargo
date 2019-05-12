5d1f224d472140871f677329a89d3e2cd20b5247
JesusDiaz08 <jesus_kdm@hotmail.com>
2019-05-11 22:07:46 -0500

    Importacion de middlewares para la API REST de TRIBAGO

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 'use strict'
 
+/**
+ * @author:      Diaz Medina Jesus Kaimorts 
+ * @version:     1.0
+ * @description: Este script tiene como objetivo encargarse de la 
+ *               importar los middlewares necesarios para el correcto
+ *               funcionamiento de nuestra aplicación API REST de TRIBAGO
+ **/
+
 const express = require('express'); // Importamos Express
 const bodyParser = require('body-parser'); // Importamos Body-parser
 const app = express(); // Agregamso express a nuestra aplicación
@@ -10,4 +18,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // En las peticiones, el cuerpo del mensaje puede ser en formato JSON
 app.use('/api', api); // Usa los archivos de configuracion del CRUD de bebida
 
-module.exports = app
+module.exports = app;
