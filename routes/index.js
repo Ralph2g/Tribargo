1eb3e46d4ca2197fe59ac59da95515eb196a871e
JesusDiaz08 <jesus_kdm@hotmail.com>
2019-05-15 15:17:00 -0500

    Checar el apartado de CRUD usuario

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 'use strict'
 
 /**
- * @author:      Diaz Medina Jesus Kaimorts
- * @version:     1.0
- * @description: En este script se encuentran los metodos necesarios para
- *               poder asignar el comportamiento del CRUD utilizando los
- *               distintos modelos creados, los cuales están contenidos,
- *               en el @package models. Se divide por secciones cada uno 
- *               de los modelos.        
+ * @author:         Diaz Medina Jesus Kaimorts
+ * @version:        1.0
+ * @description:    En este script se encuentran los metodos necesarios para
+ *                  poder asignar el comportamiento del CRUD utilizando los
+ *                  distintos modelos creados, los cuales están contenidos,
+ *                  en el @package models. Se divide por secciones cada uno 
+ *                  de los modelos.
+ * @summary:        Se debe de checar el ingreso de los usuarios             
  **/
 
 const express = require('express');
 const BebidaCtrl = require('../controllers/bebida');
+const UsuarioCtrl = require('../controllers/user');
 const auth = require('../middlewares/auth');
 const api = express.Router();
 
@@ -26,5 +28,14 @@ api.delete('/bebida/:bebidaID', BebidaCtrl.deleteBebida);
 api.get('/private', auth, (req, res) => {
     res.status(200).send({ message: 'Tienes acceso.' });
 });
+
+/**
+ * @summary:        Este CRUD no funciona bien. 
+ *                  Ignorar esta implementacion
+ */
+/* |=========== CRUD en la coleccion Persona ===========| */
+
+api.post('/signup', UsuarioCtrl.signUp);
+api.post('/signin', UsuarioCtrl.signIn);
 
 module.exports = api;
