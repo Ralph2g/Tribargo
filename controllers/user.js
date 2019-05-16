7a30d906d053d81e9bb5a24a828c526cb94958e7
JesusDiaz08 <jesus_kdm@hotmail.com>
2019-05-15 22:00:44 -0500

    Registro e Inicio de sesion (Contraseña en texto plano) :fire:

--- controllers/user.js
+++ controllers/user.js
@@ -6,74 +6,107 @@
  * @description: Este script tiene como objetivo encargarse de la 
  *               autenticación del registro e inicio de sesion por
  *               cada uno de los usuarios en nuestra API REST de TRIBARGO
- * @summary      Esta clase necesita ser cambiada, ya que a la hora de realizar
- *               el inicio de sesión y registro no hace nada y no crea la cuenta
- *               del usuario que se ha intentado registrar
  **/
 
+const User = require('../middlewares/auth');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('../config/properties');
 const ResponseHTTP = require('./codes_http');
-const Persona = require('../models/persona');
-const service = require('../services/index');
 
 /**
- * @name:           signUp
+ * @name:           createUser
  * @param:          req:    Objeto HTTP request.
  * @param:          res:    Objeto HTTP response.
+ * @param:          next:   Middleware
  * @description:    Realiza el registro de un usuario utilizando tokens
  *                  y servicios (funciones) que nos ayuden a repetir 
  *                  determinadas acciones.
  **/
-const signUp = (req, res) => {
-    const usuario = new Persona({
+function createUser(req, res, next) {
+    // Estos son los datos que se le solicitan a la hora del registro
+    const newUser = {
         nombre: req.body.nombre,
         apellido: req.body.apellido,
-        correo: req.body.correo,
-        contrasenha: req.body.contrasenha,
         edad: req.body.edad,
         sexo: req.body.sexo,
         fecha_nacimiento: req.body.fecha_nacimiento,
-        presupuesto: req.body.presupuesto
-    });
+        presupuesto: req.body.presupuesto,
+        correo: req.body.correo,
+        contrasenha: req.body.contrasenha
+    };
 
-    usuario.avatar = usuario.gravatar();
-
-    usuario.save((err) => {
-        if (err) res.status(ResponseHTTP.server_error_codes['ISE']).send({ message: `Error al crear al usuario: ${err}` });
-        return res.status(ResponseHTTP.accept_codes['Created']).send({ token: service.createToken(usuario) });
+    User.create(newUser, (err, user) => {
+        if (err)
+            return res.status(ResponseHTTP.server_error_codes['ISE']).send({ message: `Server error:${err}` });
+        const expireIn = 24 * 60 * 60; // La sesión expira en 1 día
+        const accessToken = jwt.sign({ id: user.id },
+            config.SECRET_TOKEN, {
+                expiresIn: expireIn
+            });
+        // Respuesta hacia nuestro front-end
+        res.send({ user });
     });
 }
 
 /**
- * @name:           signIn
- * @param:          req:        Objeto HTTP request
- * @param:          res:        Objeto HTTP response
- * @param:          usuario:    A este se le crea un token    
- * @description:    Busca en la base de datos, en la coleccion Persona, los
- *                  los usuarios que tengan el email pasemos en la peticion. 
- *                  Si el usuario existe, se dará acceso creando un token 
- *                  el cual viajará en la cabecera.                  
+ * @name:           loginUser
+ * @param:          req:    Objeto HTTP request.
+ * @param:          res:    Objeto HTTP response.
+ * @param:          next:   Middleware
+ * @description:    Realiza el inicio de sesion de un usuario utilizando tokens
+ *                  y servicios (funciones) que nos ayuden a repetir 
+ *                  determinadas acciones.
  **/
-const signIn = (req, res) => {
+function loginUser(req, res, next) {
+    // Estos son los datos que se pedirán para inciar sesion
+    const userData = {
+        correo: req.body.correo,
+        contrasenha: req.body.contrasenha
+    };
 
-    Persona.findOne({ correo: req.body.correo }, (err, usuario) => {
-        if (err) return res.status(ResponseHTTP.server_error_codes['ISE']).send({ msg: `Error al ingresar en la aplicación: ${err}` });
-        if (!usuario) return res.status(ResponseHTTP.client_error_codes['NotFound']).send({ msg: `No existe el usuario: ${req.body.correo}` });
+    // Se busca en la base de datos para ver si el usuario existe.
+    User.findOne({ correo: userData.correo }, (err, user) => {
+        if (err)
+            return res.status(ResponseHTTP.server_error_codes['ISE']).send({ message: `Server error: ${err}` });
+
+        if (!user) // El correo no existe o esta mal
+            res.status(ResponseHTTP.client_error_codes['Conflict']).send({ message: 'Something is wrong' }); // El usuario no se encontró
+        else {
+            const resultPassword = userData.contrasenha; // Recuperamos la contraseña del usuario
+
+            /**
+             * @summary:    Verificamos si la contraseña es la correcta.
+             *              Si el resultPassword esta descifrado, nos va a 
+             *              devolver un valor de 'false' en caso que no coincida
+             *              y 'true' en el caso de que sí.
+             */
+            if (resultPassword) {
+                const expireIn = 24 * 60 * 60; // La sesión expira en 1 día
+                const accessToken = jwt.sign({ id: user.id },
+                    config.SECRET_TOKEN, {
+                        expiresIn: expireIn
+                    });
+                res.send({ userData });
+            } else { // La contraseña no es la correcto
+                res.status(ResponseHTTP.client_error_codes['Conflict']).send({ message: 'Something is wrong' }); // El usuario no es correcto
+            } //cierra else resultPassword
+        } //cierra else de user
     });
+}
 
-    return usuario.comparePassword(req.body.contrasenha, (err, isMatch) => {
-        if (err) return res.status(ResponseHTTP.server_error_codes['ISE']).send({ msg: `Error al ingresar: ${err}` });
-        if (!isMatch) return res.status(ResponseHTTP.client_error_codes['NotFound']).send({ msg: `Error de contraseña: ${req.body.correo}` });
-
-        req.usuario = usuario;
-
-        return res.status(ResponseHTTP.server_error_codes['ISE']).send({
-            msg: 'Has ingresado de manera exitosa',
-            token: service.createToken(usuario)
-        });
-    }).select('_id email +password');
+/**
+ * @name:           greeting
+ * @param:          req:    Objeto HTTP request.
+ * @param:          res:    Objeto HTTP response.
+ * @description:    Esta funcion manda un saludo
+ **/
+function greeting(req, res) {
+    res.send('Hello from greeting function');
 }
 
 module.exports = {
-    signUp,
-    signIn
+    createUser,
+    loginUser,
+    greeting
 }
