d7bde9664552a68d7e03d6f3a4e3e85bfee8c7fd
JesusDiaz08 <jesus_kdm@hotmail.com>
2019-05-15 22:03:01 -0500

    Registro e inicio de sesion listo (con contraseña en texto plano)

--- routes/index.js
+++ routes/index.js
@@ -7,35 +7,38 @@
  *                  poder asignar el comportamiento del CRUD utilizando los
  *                  distintos modelos creados, los cuales están contenidos,
  *                  en el @package models. Se divide por secciones cada uno 
- *                  de los modelos.
- * @summary:        Se debe de checar el ingreso de los usuarios             
+ *                  de los modelos.             
  **/
 
 const express = require('express');
-const BebidaCtrl = require('../controllers/bebida');
-const UsuarioCtrl = require('../controllers/user');
-const auth = require('../middlewares/auth');
 const api = express.Router();
 
-/* |=========== CRUD en la coleccion BEBIDA ===========| */
+/**
+ * @summary:    Esquemas de las bases de datos
+ */
+const BebidaCtrl = require('../controllers/bebida');
+const Users = require('../controllers/user');
+
+/** |=========== CRUD en la coleccion BEBIDA ===========|
+ * @description:    Utilizando la api dada por ROUTER de 
+ *                  express, utilazamos las funciones para
+ *                  poder crear el CRUD de la colección 
+ *                  'Bebida' en la base de datos.
+ */
 api.get('/bebida', BebidaCtrl.getBebidas);
 api.post('/bebida', BebidaCtrl.saveBebida);
 api.get('/bebida/:bebidaID', BebidaCtrl.getBebida);
 api.put('/bebida/:bebidaID', BebidaCtrl.updateBebida);
 api.delete('/bebida/:bebidaID', BebidaCtrl.deleteBebida);
 
-// es de prueba este
-api.get('/private', auth, (req, res) => {
-    res.status(200).send({ message: 'Tienes acceso.' });
-});
-
-/**
- * @summary:        Este CRUD no funciona bien. 
- *                  Ignorar esta implementacion
- */
-/* |=========== CRUD en la coleccion Persona ===========| */
-
-api.post('/signup', UsuarioCtrl.signUp);
-api.post('/signin', UsuarioCtrl.signIn);
+/** |=========== Registro e inicio de sesión ===========|
+ * @description:    Las funciones que a continación se muestran, permiten
+ *                  el registro e inicio de sesión del usuario utilizando
+ *                  el esquema previamente diseñado en el @package 
+ *                  /models/persona.js 
+ **/
+api.post('/register', Users.createUser);
+api.post('/login', Users.loginUser);
+api.get('/saludo', Users.greeting);
 
 module.exports = api;
