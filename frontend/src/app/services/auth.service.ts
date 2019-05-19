47b07067ef65a3fe40507633d7c15b78ca79c6c5
JesusDiaz08 <jesus_kdm@hotmail.com>
2019-05-18 22:49:03 -0500

    Aqui estan las funciones Registro, Inicio de sesion, guardar token y obtener token

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserI } from '../models/user';
+import { JwtResponseI } from '../models/jwt-response';
+import { tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class AuthService {
+  public PUERTO:number = 3001;
+  AUTH_SERVER:string = `http://localhost:${this.PUERTO}/api`;
+  authSubject = new BehaviorSubject(false);
+  private token: string;
+
+  constructor( private httpClient: HttpClient) { }
+
+  register( usuario:UserI ):Observable<JwtResponseI>{
+    return this.httpClient.post<JwtResponseI>(`${this.AUTH_SERVER}/register`, usuario).pipe(
+      tap( (res: JwtResponseI) => {
+        if(res){
+          // Guardar Token
+          this.saveToken(res.dataUser.accessToken, res.dataUser.expiresIn);
+        }
+      })
+    );
+  }// cierra metodo register
+
+  login( usuario:UserI ):Observable<JwtResponseI>{
+    return this.httpClient.post<JwtResponseI>(`${this.AUTH_SERVER}/login`, usuario).pipe(
+      tap( (res: JwtResponseI) => {
+        if(res){
+          // Guardar Token
+          this.saveToken(res.dataUser.accessToken, res.dataUser.expiresIn);
+        }
+      })
+    );
+  }// cierra metodo login
+
+  logout():void {
+    this.token = '';
+    localStorage.removeItem('ACCESS_TOKEN');
+    localStorage.removeItem('EXPIRES_IN');
+  }// cierra logout
+
+  private saveToken( token_: string, expiresIn_: string):void{
+    localStorage.setItem('ACCESS_TOKEN', token_);
+    localStorage.setItem('EXPIRES_IN', expiresIn_);
+    this.token = token_;
+  }// cierra saveToken
+
+  private getToken():string{
+    if( this.token ){
+      this.token = localStorage.getItem('ACCESS_TOKEN');
+    }
+    return this.token;
+  }
+
+}
