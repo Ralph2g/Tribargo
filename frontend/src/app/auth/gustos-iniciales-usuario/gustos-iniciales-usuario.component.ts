24c2b63f25093ad912cdb6c8e2b8b3d6ca14822a
AlexGutierrez00 <43190606+AlexGutierrez00@users.noreply.github.com>
2019-05-27 20:52:18 -0500

    Generación de la cadena para el algoritmo que predice
    Agregados los campos de frontend para presupuesto, genero, sexo, ofertas y bares asi como un boton para generar la cadena que se enviara por al algoritmo, imprime la cadena pero puede omitirse esa parte del codigo (viene documentada esa parte)

--- frontend/src/app/auth/gustos-iniciales-usuario/gustos-iniciales-usuario.component.ts
+++ frontend/src/app/auth/gustos-iniciales-usuario/gustos-iniciales-usuario.component.ts
@@ -10,14 +10,16 @@ export class GustosInicialesUsuarioComponent implements OnInit {
   
   marks: string [] = ['Cerveza','Whiskey','Ron','Vodka','Cognac','Vino','Brandy','Tequila'];
   genres: string [] = ['Reggaeton','Salsa','Banda', 'Rock', 'Cumbia','Nortenha','Trap','Electronica','House','Hip hop','Rap','Merengue','Ranchera','Disco','Bachata','Corrido'];
+  offerts: string [] = ['2x1','Chicas gratis','Cumpleanero','Sin cover']; 
   selection1: string [] = [];
   selection2: string [] = [];
-  salida1: string [] = [];
-  salida2: string [] = [];
-  constructor(){
+  selection3: string [] = [];
+  cadenafinal: string [] = [''];
+  presupuesto: string = '500';
+  edad: string = '18';
+  sexo: string = 'masculino';
+  bar: string ='Brazilian Terraza & Grill';
 
-  }
-  
   public insertMark(mark){
     let repetir = 0;
     for(let i=0; i< this.selection1.length; i++)
@@ -48,37 +50,18 @@ export class GustosInicialesUsuarioComponent implements OnInit {
     }
   }
 
-  public generaSal1(){
-    for(let k=0; k < this.marks.length;k++)  
+  public insertOffert(offert){
+    let repetir = 0;
+    for(let i=0; i< this.selection3.length; i++)
     {
-      this.salida1[k] = '0';
-    }
-    for(let j=0; j<this.selection1.length;j++)
-    {
-      for(let i=0; i< this.marks.length; i++)
+      if(offert == this.selection3[i])
       {
-        if(this.selection1[j] == this.marks[i])
-        {
-          this.salida1[i] = '1';
-        }
-      }
+        repetir = 1;
+      }     
     }
-  }
-
-   public generaSal2(){
-    for(let k=0; k < this.genres.length;k++)  
+    if(repetir == 0 )
     {
-      this.salida2[k] = '0';
-    }
-    for(let j=0; j<this.selection2.length;j++)
-    {
-      for(let i=0; i< this.genres.length; i++)
-      {
-        if(this.selection2[j] == this.genres[i])
-        {
-          this.salida2[i] = '1';
-        }
-      }
+      this.selection3.push(offert);
     }
   }
   
@@ -91,6 +74,7 @@ export class GustosInicialesUsuarioComponent implements OnInit {
       }
     }
   }
+
   public deleteGenre(select2){
     for(let i=0; i< this.selection2.length; i++)
     {
@@ -99,6 +83,84 @@ export class GustosInicialesUsuarioComponent implements OnInit {
         this.selection2.splice(i,1);      
       }
     }
+  }
+
+  public deleteOffert(select3){
+    for(let i=0; i< this.selection3.length; i++)
+    {
+      if(select3 == this.selection3[i])
+      {
+        this.selection3.splice(i,1);      
+      }
+    }
+  }
+
+  public generaCadenaFinal(){
+    //Insertar el presupuesto
+    this.cadenafinal.push(this.presupuesto);
+    this.cadenafinal.push('@');
+    //Insertar la edad
+    this.cadenafinal.push(this.edad);
+    this.cadenafinal.push('@');
+    //Insertar el sexo
+    this.cadenafinal.push(this.sexo);
+    this.cadenafinal.push('@');
+    //Insertar gustos en bebidas
+     /* select1 */
+    for(let k=0; k < this.marks.length;k++)  
+    {
+      this.cadenafinal[k+7] = '0';
+    }
+    for(let j=0; j<this.selection1.length;j++)
+    {
+      for(let i=0; i< this.marks.length; i++)
+      {
+        if(this.selection1[j] == this.marks[i])
+        {
+          this.cadenafinal[i+7] = '1';
+        }
+      }
+    }
+     /* */
+    this.cadenafinal.push('@');
+    //Insertar gustos en musica
+    /* */
+    for(let k=0; k < this.genres.length;k++)  
+    {
+      this.cadenafinal[k+16] = '0';
+    }
+    for(let j=0; j<this.selection2.length;j++)
+    {
+      for(let i=0; i< this.genres.length; i++)
+      {
+        if(this.selection2[j] == this.genres[i])
+        {
+          this.cadenafinal[i+16] = '1';
+        }
+      }
+    }
+     /* select2 */
+    this.cadenafinal.push('@');
+    //Insertar gustos en ofertas
+     /* select 3*/
+     for(let k=0; k < this.offerts.length;k++)  
+    {
+      this.cadenafinal[k+33] = '0';
+    }
+    for(let j=0; j<this.selection3.length;j++)
+    {
+      for(let i=0; i< this.offerts.length; i++)
+      {
+        if(this.selection3[j] == this.offerts[i])
+        {
+          this.cadenafinal[i+33] = '1';
+        }
+      }
+    }
+     /* */
+    this.cadenafinal.push('@');
+    //Insertar bar
+    this.cadenafinal.push(this.bar);
   }
 
   ngOnInit() {
