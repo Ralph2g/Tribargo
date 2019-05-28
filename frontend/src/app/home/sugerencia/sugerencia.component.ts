ed300f22afbdb01b8760f540708e8c5d0b9ba266
Rafael Garcia Garcia <37715000+Ralph2g@users.noreply.github.com>
2019-05-28 00:25:53 -0500

    Maquetaci´´on funcional de los bares sugeridos

--- frontend/src/app/home/sugerencia/sugerencia.component.ts
+++ frontend/src/app/home/sugerencia/sugerencia.component.ts
@@ -7,8 +7,11 @@ import { ActivatedRoute, Router, Params } from '@angular/router';
   styleUrls: ['./sugerencia.component.css']
 })
 export class SugerenciaComponent implements OnInit {
-
+  public iconMap = {
+    iconUrl:'http://localhost:4200/assets/images/icons/beer-icon.png',
+  }
   public id:number;//id del bar para sacarlo de la base datos
+  public name:string;
   public score:number;//puntuacion
   public price:number;//precio
   public image:File;//imagen
@@ -28,11 +31,25 @@ export class SugerenciaComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
     this._route.params.forEach((params: Params) =>{
-      let id =params['id'];  
-      this.id = id;
+    let id =params['id'];  
+    this.id = id;
       
     });
+    this.name = 'Terraza-Madero';
+    this.latitude = 19.433869;
+    this.longitude = -99.138893;
+    this.score = 4;
+    this.price = 150;
+    this.image = null;
+    this.drinks = ['Heineken','XX','victoria'];
+    this.snaks = ['Alitas','Hamburguesas', 'Papas'];
+    this.music = ['Regueton', 'Banda','Salsa', 'ska'];
+    this.description = 'Somos el bar más popular para pasar la tarde con tus amigos, hay de todo y a buen precio';
+    this.promos = ['2x1','No-cover'];
+    this.location = 'Av Francisco I. Madero 20, Centro Histórico de la Cdad. de México, Centro, 06010 Ciudad de México, CDMX';
+
   }
 
 }
