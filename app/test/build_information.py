b7e783da00b368bcd1eaaa63d8f5fed1b7372988
JesusDiaz08 <jesus_kdm@hotmail.com>
2019-05-30 23:12:51 -0500

    build_information

--- app/test/build_information.py
+++ app/test/build_information.py
@@ -10,19 +10,25 @@ def descomponer( cadena ):
 def adaptar( preferencias ):
     presupuesto, edad, sexo, bebidas,musica, promos, bar = preferencias.split("@")
     lista_bebidas = descomponer(bebidas)
-    lista_musica = descomponer(music)
-    lista_bebidas = descomponer(promos)
+    lista_musica = descomponer(musica)
+    lista_promo = descomponer(promos)
 
-    auxiliar = { "presupuesto" : presupuesto.replace(',', '').strip(),
-                 "edad" : edad.replace(',', '').strip(),
-                 "sexo" : "femenino" if sexo.replace(',', '').strip() == "0" else "masculino" ,
-                 "bebidas" : ,
-                 "musica" : ,
-                 "promos" : ,
-                 "bar" : bar.replace(',', '').strip() 
+    presupuesto = presupuesto.replace(',', '').strip()
+    edad = edad.replace(',', '').strip()
+    sexo = "femenino" if sexo.replace(',', '').strip() == "0" else "masculino"
+    bar = bar.replace(',', '').strip()
+
+    auxiliar = { "presupuesto" : presupuesto,
+                 "edad" : edad,
+                 "sexo" :  sexo ,
+                 "bebidas" : lista_bebidas[0],
+                 "musica" : lista_musica[0],
+                 "promos" : lista_promo[0],
+                 "bar" :  bar
                 }
-    s = pd.DataFrame(auxiliar)
-    print(s)
+    s = pd.Series(auxiliar)
+    p = pd.DataFrame(s)
+    print(p)
 
 preferencias = "1593,@,23,@,1,@,0,1,1,@,1,1,0,1,@,0,1,1,@,Mr Duck"
 adaptar(preferencias)
